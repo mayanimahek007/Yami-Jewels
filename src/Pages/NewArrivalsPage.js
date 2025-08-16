ff1740bf9d2eaa1217b63d299042201aecf59be2
mayanimahek007 <mayanimahek007@gmail.com>
2025-08-16 15:07:57 +0530

    add

--- src/Pages/NewArrivalsPage.js
+++ src/Pages/NewArrivalsPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './newArrivals.css';
 import { IoDiamond } from 'react-icons/io5';
-import bannerImg from '../assets/images/webp/img2.webp';
+import bannerImg from '../assets/images/webp/new arrivals.webp';
 
 const categories = [
   { name: 'Bracelet', img: require('../assets/images/bracelets1.png') },
@@ -11,7 +11,7 @@ const categories = [
 
 const NewArrivalsPage = () => {
   return (
-    <div className="min-h-screen bg-[#fdf8f8] flex flex-col items-center pt-0 newArrib_pag">
+    <div className="bg-[#fdf8f8] flex flex-col items-center pt-0 newArrib_pag">
       <div className="w-full flex flex-col items-center justify-center relative">
         {/* Banner */}
         <img
@@ -21,8 +21,8 @@ const NewArrivalsPage = () => {
         />
 
         {/* Overlay Text */}
-        <div className="w-full flex flex-col justify-center sm:pl-16 text-white absolute top-0 left-0 sm:pt-16 lg:pt-20 pt-4 pl-4 z-10">
-          <div className='flex items-center justify-start gap-3 mb-3'>
+        <div className="w-full flex flex-col justify-center sm:pl-16 gap-3 sm:gap-5 text-white absolute top-0 left-0 sm:pt-16 lg:pt-20 pt-4 pl-4 z-10">
+          <div className='flex items-center justify-start gap-3 '>
             <h1 className="text-md sm:text-lg md:text-2xl lg:text-5xl font-medium font-playfair drop-shadow text-[#000000]">
               New Arrivals
             </h1>
@@ -33,35 +33,14 @@ const NewArrivalsPage = () => {
           <p className="text-[11px] sm:text-lg font-medium text-[#000000] drop-shadow mb-1">
             New Arrivals Dropping Daily, Monday through Friday.
           </p>
-          <p className="text-xs sm:text-md font-normal text-[#000000] drop-shadow">
+          <p className="text-xs sm:text-base font-normal text-[#000000] drop-shadow">
             Explore the Latest Launches Now!
           </p>
+            <button className="bg-[#48182E] text-white text-xs mt-0 sm:mt-5 sm:text-sm md:text-base px-4 py-2 rounded-md shadow-md hover:bg-[#6a2545] transition w-fit">
+            Shop Now
+          </button>
         </div>
 
-        {/* Categories Grid (overlapping 40% inside banner) */}
-        <div className="absolute left-1/2 bottom-0 translate-y-[60%] -translate-x-1/2 w-full max-w-[1100px] px-4 z-20 new_arrvls_crd">
-          {/* <div className="w-full flex justify-center"> */}
-            <div className="flex w-full max-w-[1100px] justify-between px-0 sm:px-8 gap-2 md:gap-4 saxsa_Dcdxc">
-              {categories.map((cat) => (
-                <div
-                  key={cat.name}
-                  className="w-1/3 sdcsdcsd bg-[#fdf8f8] rounded-2xl p-1 md:p-2 flex flex-col items-center"
-                >
-                  <div className="relative w-full aspect-square overflow-hidden rounded-2xl shadow-md transition-transform">
-                    <img
-                      src={cat.img}
-                      alt={cat.name}
-                      className="absolute inset-0 w-full h-full object-cover z-0"
-                    />
-                    <div className="absolute inset-0 bg-gradient-to-t from-[#00000070] to-transparent z-10" />
-                  
-                  </div>
-                </div>
-              ))}
-            {/* </div> */}
-          </div>
-
-        </div>
       </div>
     </div>
   );
